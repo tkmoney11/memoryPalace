cdc8e19f493afe61ad3ff588f65beba121ea2db1
annieztang <annie.z.tang@gmail.com>
2020-07-01 20:48:46 -0700

    add landing page for desktop only

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="home">
+      <h1 id="title1">WELCOME TO YOUR</h1>
+      <h1 id="title2">MEMORY PALACE</h1>
+      <div id="button-container">
+        <div className="button">Begin</div>
+        <a className="button" href="https://en.wikipedia.org/wiki/Method_of_loci" target="_blank" rel="noopener noreferrer"><div id="learn-more">Learn More</div></a>
+      </div>
     </div>
   );
 }
